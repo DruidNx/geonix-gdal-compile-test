969ad233111d09d11ba16d25ed1582a14d3e3c39
DruidNx <druid.bng@gmail.com>
2024-03-18 14:08:50 +0100

    Suggested changes to override glibc and gcc

--- flake.nix
+++ flake.nix
@@ -27,26 +27,67 @@
     mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
 
-      imports = [
-        inputs.devenv.flakeModule
-      ];
+      imports = [ inputs.devenv.flakeModule ];
 
       systems = [ "x86_64-linux" ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
 
-        devenv.shells.default = {
-          imports = [
-              inputs.geonix.modules
-            ./geonix.nix
-          ];
+        let
+
+          glibc_2_27 =
+            (import (builtins.fetchGit {
+              name = "glibc_2_18";
+              url = "https://github.com/NixOS/nixpkgs/";
+              ref = "refs/heads/nixos-19.09";
+              rev = "b79f64b5eb5fa8ca2f844ddb4d7c186b6c69a293";
+            }) { inherit system; }).glibc;
+
+          gcc_8_3_0 =
+            (import (builtins.fetchGit {
+              name = "gcc_8_3_0";
+              url = "https://github.com/NixOS/nixpkgs/";
+              ref = "refs/heads/nixpkgs-unstable";
+              rev = "a9eb3eed170fa916e0a8364e5227ee661af76fde";
+            }) { inherit system; }).gcc-unwrapped;
+
+          getCustomGccStdenv =
+            customGcc: customGlibc: origStdenv:
+            { pkgs, ... }:
+            with pkgs;
+            let
+              compilerWrapped = wrapCCWith {
+                cc = customGcc;
+                bintools = wrapBintoolsWith {
+                  bintools = binutils-unwrapped;
+                  libc = customGlibc;
+                };
+              };
+            in
+            overrideCC origStdenv compilerWrapped;
+          gcc_8_3_0_glibc_2_27 = getCustomGccStdenv gcc_8_3_0 glibc_2_27 pkgs.stdenv pkgs;
+        in
+
+        {
+
+          packages.geonixcli = inputs.geonix.packages.${system}.geonixcli;
+
+          packages.gdal = inputs.geonix.packages.${system}.gdal.overrideAttrs (
+            finalAttrs: previousAttrs: { stdenv = gcc_8_3_0_glibc_2_27; }
+          );
         };
-
-        packages.geonixcli = inputs.geonix.packages.${system}.geonixcli;
-      };
 
       flake = { };
     };
